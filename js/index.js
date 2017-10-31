6cd74c5edb1ee396dff6e98deef11e81d353f23f
lizhihao <m18939295085@163.com>
2017-10-31 13:38:56 +0800

    xinde

---
+++ js/index.js
@@ -0,0 +1,80 @@
+/**
+ * Created by Administrator on 2017/9/30.
+ */
+'use strict';
+$(function () {
+    /*swiper*/
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        loop: true,
+        autoplay: 5000
+    });
+    /*fullpage*/
+    $('#dookbook').fullpage({
+        anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7'],
+        scrollingSpeed: 1000,
+        // keyboardScrolling: false,
+        // scrollBar: true,
+        menu: '#menu',
+        onLeave: function (index, nextIndex, direction) {
+//                console.log(nextIndex + "=----" + direction)
+
+            if (nextIndex > 1) {
+                if (nextIndex == 2 && direction == 'down') {
+                    setTimeout(function () {
+                        $("#menu").show();
+                    }, 100);
+                } else {
+                    $("#menu").show();
+                }
+            } else {
+                $("#menu").hide();
+            }
+        }
+    });
+    /*右边栏*/
+    $('#menu li ul li').on('click', function () {
+        $(this).addClass('active');
+        $(this).siblings().removeClass('active');
+        var ind = $(this).index();
+        $('.book').find('.phy-waiguo').eq(ind).addClass('active').siblings().removeClass('active')
+    });
+    $('#menu li ul').css('display', 'none');
+    $('#menu li a').on('click', function () {
+        $(this).next('ul').show();
+        $(this).parent('li').siblings().find('ul').hide();
+    });
+    $('#menu li ul li').on('click', function () {
+        $(this).addClass('active');
+        $(this).siblings().removeClass('active');
+    });
+    /*图书详情*/
+    $('.phy-waiguo img').on('click', function () {
+        $('.phy-con').show();
+    });
+    $('.show-title').on('click', function () {
+        $('.phy-con').hide();
+    });
+    /*IP详情*/
+    $('.ip-aj').on('click', function () {
+        $('.super-show').show();
+    });
+    $('.sups-title').on('click', function () {
+        $('.super-show').hide()
+    });
+    /*作家详情*/
+    $('.author ul li img').on('click', function () {
+        $('.author-show').show();
+    });
+    $('.author-title').on('click', function () {
+        $('.author-show').hide();
+    });
+    /*招聘详情*/
+    $('.adds-title ul li').on('click', function () {
+        $(this).addClass('active');
+        $(this).siblings().removeClass('active');
+        var inds = $(this).index();
+        $('.adds-bottom').eq(inds).addClass('active').siblings().removeClass('active')
+    });
+});
